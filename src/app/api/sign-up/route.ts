c817c4ec738e9ab7f92fee49f1e7baa9478a82c8
Siddharth Sarkar <sidsarkar2112@gmail.com>
2026-04-09 09:55:02 +0530

    feat: initial commit

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -2,22 +2,30 @@ import dbConnect from "@/lib/dbConnect";
 import TeamLeaderModel from "@/models/teamLeader";
 import { signUpSchema } from "@/schemas/signUp";
 import ErrorHandler from "@/utils/errorHandler";
+import { sendVerificationEmail } from "@/utils/sendVerificationMail";
 import { wrapAsync } from "@/utils/wrapAsync";
 import bcrypt from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
-import { success } from "zod";
+
+
+function ToUpperCase(str: string) {
+  return str.at(0)?.toUpperCase() + str.substring(1);
+}
 
 export const POST = wrapAsync(async (req: NextRequest) => {
   await dbConnect();
 
   const body = await req.json();
-  const { name, email, password } = body;
-  if (!name || !email || !password) {
+  const { firstName, lastName, email, password } = body;
+
+  if (!firstName || !lastName || !email || !password) {
     throw new ErrorHandler("Please Provide All Details", 400);
   }
 
+  const name = ToUpperCase(firstName) + " " + ToUpperCase(lastName);
+
   // zod validation
-  const checkSignUpSchema = signUpSchema.safeParse(body);
+  const checkSignUpSchema = signUpSchema.safeParse({ name, email, password });
   if (!checkSignUpSchema.success) {
     const formatted = checkSignUpSchema.error.format();
 
@@ -31,11 +39,31 @@ export const POST = wrapAsync(async (req: NextRequest) => {
     );
   }
 
-  const userExist = await TeamLeaderModel.findOne({email})
-  if(userExist){
-    throw new ErrorHandler("User alreay registered with this Email ID",400)
+  const userExist = await TeamLeaderModel.findOne({ email });
+  if (userExist) {
+    throw new ErrorHandler("User alreay registered with this Email ID", 400);
   }
 
-  const hashPass = await bcrypt.hash(password,10);
-  const 
+  const verifyCode: number = Math.ceil(100000 + Math.random() * 999999);
+  console.log(verifyCode);
+  const hashPass = await bcrypt.hash(password, 10);
+  const verifyCodeExpiry = new Date(Date.now() + 5 * 60 * 1000);
+
+//   const user = await TeamLeaderModel.create({
+//     name,
+//     email,
+//     password: hashPass,
+//     verifyCode,
+//     verifyCodeExpiry,
+//   });
+
+  const emailResponse = await sendVerificationEmail(email, name, verifyCode);
+  if(!emailResponse.success){
+    toast
+    throw new ErrorHandler(emailResponse.message, 500)
+  }
+
+
+
+  return NextResponse.json({ success: true, message: "" }, { status: 200 });
 });
---
+++ src/utils/sendVerificationMail.ts
@@ -0,0 +1,62 @@
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+  email: string,
+  name: string,
+  verifiyCode: number,
+): Promise<ApiResponse> {
+  try {
+    const repo = await fetch("https://api.brevo.com/v3/smtp/email", {
+      method: "",
+      headers: {
+        "Content-Type": "application/json",
+        "api-key": process.env.BREVO_API_KEY!,
+      },
+      body: JSON.stringify({
+        sender: {
+          email: process.env.BRROVO_SENDER_EMAIL,
+          name: process.env.BREBO_SENDER_NAME,
+        },
+        to: [{ email }],
+        subject: "Verification Code",
+        htmlContent: `
+                <div style="font-family: sans-serif; max-width: 400px; margin: auto;">
+                    <h2>Hello ${name}!</h2>
+                    <p>Your OTP verification code is:</p>
+                    <div style="
+                    font-size: 36px;
+                    font-weight: bold;
+                    letter-spacing: 8px;
+                    color: #4F46E5;
+                    padding: 16px;
+                    background: #F3F4F6;
+                    border-radius: 8px;
+                    text-align: center;
+                    ">
+                    ${verifiyCode}
+                    </div>
+                    <p style="color: #6B7280; font-size: 14px;">
+                    Expires in 5 minutes. Do not share it.
+                    </p>
+                </div>
+                `,
+      }),
+    });
+
+    if (!repo.ok) {
+      return {
+        success: false,
+        message: "Failed to send verification email",
+      };
+    }
+    return {
+      success: true,
+      message: "Verificaiton Email send successfully",
+    };
+  } catch (err) {
+    return {
+      success: false,
+      message: "Failed to send verification email",
+    };
+  }
+}
